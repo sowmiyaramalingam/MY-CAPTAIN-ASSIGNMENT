efd792267bc22819531245a6163c3e5f676ea0b9
sowmiyaramalingam <99523395+sowmiyaramalingam@users.noreply.github.com>
2022-03-14 09:12:50 +0530

    Add files via upload

--- abc.py
+++ abc.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 radius =float(input("Enter the radius of the circle"))
 area =3.14*radius*radius
 print("area of the circle is: ",area)
 
-filename =input("Input filename")
+filename =input("Enter filename")
 f_extens =filename.split(".")
-print("extension of the file is:"+repr(f_extens[-1]))
+print("extension of the file is:",f_extens[-1])
